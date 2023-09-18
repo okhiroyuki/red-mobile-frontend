4ba857bbc48c4c2676b9b43de82b8f6eeaa92f52
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2023-09-18 15:06:46 +0900

    fix: modify navlist

--- src/components/NavList.vue
+++ src/components/NavList.vue
@@ -1,14 +1,16 @@
 <template>
   <v-list nav>
     <v-list-item>
-      <v-avatar size="36px">
-        <v-img alt="Avatar" src="/src/assets/logo.png" />
-      </v-avatar>
+      <template v-slot:prepend>
+        <v-avatar size="36px">
+          <v-img alt="Avatar" src="/src/assets/logo.png" />
+        </v-avatar>
+      </template>
       <v-list-item-content> RedMobile </v-list-item-content>
     </v-list-item>
     <v-divider />
 
-    <v-list-item-group v-model="selectedItem" active-class="text--accent-4">
+    <v-list v-model="selectedItem">
       <v-list-item
         v-for="(item, i) in items"
         :key="i"
@@ -32,7 +34,7 @@
           {{ getVersion }}
         </v-list-item-title>
       </v-list-item>
-    </v-list-item-group>
+    </v-list>
   </v-list>
 </template>
 

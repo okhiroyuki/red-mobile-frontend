d94e7058e530079e9f50d0098333908c8bf60aa4
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2023-09-19 22:45:37 +0900

    fix: node-polyfills

--- vite.config.js
+++ vite.config.js
@@ -3,35 +3,17 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [vue(), vuetify({ autoImport: true }), nodePolyfills()],
   resolve: {
     alias: {
-      path: "rollup-plugin-node-polyfills/polyfills/path",
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   build: {
     outDir: "src-cordova/dist",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-    },
   },
 });

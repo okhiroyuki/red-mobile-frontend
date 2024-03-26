8c615a973a4ff6b2422f51fd90deb592278cfa34
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-03-26 21:23:18 +0900

    feat: update devcontainer

--- .devcontainer/update-content-command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git config --global url.https://x-access-token:${GITHUB_TOKEN}@github.com/.insteadOf ssh://git@github.com/
-git config --global url.https://x-access-token:${GITHUB_TOKEN}@github.com/.insteadOf git@github.com/
-git config --global url.https://x-access-token:${GITHUB_TOKEN}@github.com/.insteadOf git@github.com:
-git submodule update --init --recursive

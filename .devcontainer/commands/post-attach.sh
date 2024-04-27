24a5bd26c9bf556037d6864871741a75473a7c93
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-04-27 10:47:56 +0000

    chore: add shellckech

--- .devcontainer/commands/post-attach.sh
+++ .devcontainer/commands/post-attach.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo chown $USER .pre-commit-cache
-sudo chown $USER .npm
-sudo chown $USER node_modules
+sudo chown "$USER" .pre-commit-cache
+sudo chown "$USER" .npm
+sudo chown "$USER" node_modules
 
 npm ci

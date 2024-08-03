1b19db22473ea80283bd7c02eb89a258ed1da14b
Hiroyuki Okada <ok.okada.hiroyuki@gmail.com>
2024-08-03 12:49:59 +0000

    feat: update devcontainer

--- .devcontainer/commands/post-attach.sh
+++ .devcontainer/commands/post-attach.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! \( -f .git/hooks/pre-commit \) ]; then
+  pre-commit install
+fi
+
 sudo chown "$USER" .pre-commit-cache
 sudo chown "$USER" .npm
 sudo chown "$USER" node_modules
